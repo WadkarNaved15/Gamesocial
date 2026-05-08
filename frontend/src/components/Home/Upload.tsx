8ca694b0371ea4e93ec4b0612ccc255d83d94eb7
WadkarNaved15 <navedwadkar9@gmail.com>
2026-05-08 20:22:46 +0530

    Solved upload button movement on scroll

--- frontend/src/components/Home/Upload.tsx
+++ frontend/src/components/Home/Upload.tsx
@@ -1,18 +1,19 @@
 import { FaUpload } from "react-icons/fa";
 
-export default function UploadBox({ onUploadClick }: { onUploadClick: () => void }) {
+export default function UploadBox({
+  onUploadClick,
+}: {
+  onUploadClick: () => void;
+}) {
   return (
-    <div className="w-full max-w-3xl mx-auto mt-8">
+    <div className="w-full max-w-3xl mx-auto mt-8 sticky top-4 z-20">
       <button
         onClick={onUploadClick}
         className="
-          min-w-full h-10 px-4 flex items-center justify-center 
+          w-full h-10 px-4 flex items-center justify-center
           rounded-lg border-[1px] transition-all duration-200
-          /* Light Mode */
           bg-[#F9FAFB] border-[#E0E0E5] text-gray-700 hover:bg-gray-100
-          /* Dark Mode */
           dark:bg-[#191919] dark:border-gray-700 dark:text-gray-200 dark:hover:bg-[#1e1e1e]
-          /* Interactive */
           active:scale-[0.98] font-medium tracking-wide
         "
       >
