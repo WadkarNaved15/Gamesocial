fc45e37f30b66e9cb32bc282719c7f030ca81c8e
usaid <sayedusaid49@gmail.com>
2026-06-17 19:25:29 +0530

    logs added

--- server/routes/internalNotification.js
+++ server/routes/internalNotification.js
@@ -4,9 +4,10 @@ import Notification from "../models/Notifications.js";
 const router = express.Router();
 
 export default function internalNotificationRoutes(io, onlineUsers) {
-  console.log("io exists?", !!io);
-  console.log("onlineUsers exists?", !!onlineUsers);
+
   router.post("/notify-realtime", async (req, res) => {
+    console.log("io exists?", !!io);
+    console.log("onlineUsers exists?", !!onlineUsers);
     console.log("notify-realtime got hit");
     console.log("Header Secret:", req.headers["x-internal-secret"]);
     console.log("Env Secret:", process.env.INTERNAL_SECRET);
