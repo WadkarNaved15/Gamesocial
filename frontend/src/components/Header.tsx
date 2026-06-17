299574b3ac9d627f5de294cfea40ad28112697bd
WadkarNaved15 <navedwadkar9@gmail.com>
2026-06-17 17:48:10 +0530

    Made ui changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -116,7 +116,7 @@ export function Header() {
               className="group relative flex items-center justify-center"
             >
               <Logo
-                className="h-12 w-auto transition-all duration-300 hover:-translate-y-[1px] text-[#29665a]" />
+                className="h-12 w-auto transition-all duration-300 hover:-translate-y-[1px] text-[#62D4AE]" />
             </Link>
           </div>
 
