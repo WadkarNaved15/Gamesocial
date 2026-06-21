3cefffc7e61e2bd3872b40d78ba9c3e310316f1a
WadkarNaved15 <navedwadkar9@gmail.com>
2026-06-21 19:32:42 +0530

    Added more logs

--- server/routes/analytics.js
+++ server/routes/analytics.js
@@ -219,6 +219,11 @@ router.post(
       console.log("geo:", geo);
       console.log("asn:", asn);
 
+      console.log("headers:", req.headers);
+console.log("req.ip:", req.ip);
+console.log("x-forwarded-for:", req.headers["x-forwarded-for"]);
+console.log("x-real-ip:", req.headers["x-real-ip"]);
+
       const sessionId =
         crypto.randomUUID();
 
