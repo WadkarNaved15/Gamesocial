b69fb03f7a98050a43f683519a7b4c6af7ad45fc
WadkarNaved15 <navedwadkar9@gmail.com>
2026-06-24 17:03:43 +0530

    Added logs

--- server/config/sqsClient.js
+++ server/config/sqsClient.js
@@ -1,4 +1,8 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
+
+console.log("SQS AWS_REGION =", process.env.AWS_REGION);
+console.log("SQS_QUEUE_URL =", process.env.SQS_QUEUE_URL);
+
 export const sqsClient = new SQSClient({
   region: process.env.AWS_REGION,
   credentials: {
