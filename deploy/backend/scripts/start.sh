4c1e9b6f8ea70b194cf8ad448de94b588b7c8d6d
WadkarNaved15 <navedwadkar9@gmail.com>
2026-06-24 14:56:58 +0530

    Added env update in script

--- deploy/backend/scripts/start.sh
+++ deploy/backend/scripts/start.sh
@@ -6,6 +6,18 @@ systemctl restart nginx
 
 cd /home/ubuntu/Rigzer/server
 
+echo "Loading environment variables from SSM..."
+
+aws ssm get-parameter \
+  --region us-east-1 \
+  --name "/rigzer/prod/env" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text > .env
+
+chown ubuntu:ubuntu .env
+chmod 600 .env
+
 sudo -u ubuntu pm2 reload index || \
 sudo -u ubuntu pm2 start index.js --name index
 
