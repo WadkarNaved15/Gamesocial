a18dd95932793bf73b41471d5ba0583312c37069
WadkarNaved15 <navedwadkar9@gmail.com>
2026-08-06 00:28:58 +0530

    Updated view worker

--- server/services/postViewRedis.js
+++ server/services/postViewRedis.js
@@ -1,19 +1,16 @@
 import redis from "../config/redis.js";
 
-const UNIQUE_TTL_SECONDS = 60 * 60 * 24; // 24 hours
-const COOLDOWN_TTL_SECONDS = 60 * 5; // 5 minutes
+const COOLDOWN_TTL_SECONDS = 60 * 15; // 15 minutes
 
 export const markViewInRedis = async ({ postId, viewerId }) => {
-  const uniqueKey = `post:view:unique:${postId}:user:${viewerId}`;
   const cooldownKey = `post:view:cooldown:${postId}:user:${viewerId}`;
 
-  const [cooldownSet, uniqueSet] = await Promise.all([
-    redis.set(cooldownKey, "1", { NX: true, EX: COOLDOWN_TTL_SECONDS }),
-    redis.set(uniqueKey, "1", { NX: true, EX: UNIQUE_TTL_SECONDS }),
-  ]);
+  const cooldownSet = await redis.set(cooldownKey, "1", {
+    NX: true,
+    EX: COOLDOWN_TTL_SECONDS,
+  });
 
   return {
     countedThisTime: Boolean(cooldownSet),
-    isUnique: Boolean(uniqueSet),
   };
 };
